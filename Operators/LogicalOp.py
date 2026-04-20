0215d6c345a2f41f598b45d993533f8da37f5c92
Harshitajain07 <harshitajain178@gmail.com>
2026-04-20 17:54:19 +0530

    Initial Commit

---
+++ Operators/LogicalOp.py
@@ -0,0 +1,31 @@
+#logical operators
+
+"""
+print(not False)
+print(not True)
+"""
+
+"""
+a = 50
+b = 30
+print(not False)
+print(not (a>b)) #as a is greater than b but before a>b there is not written
+"""
+
+#AND operator
+"""
+val1 = True
+val2 = True
+print("AND Operator: ", val1 and val2) 
+"""
+"""a) if both values are true then the output is also true
+    b) if any one of them is false then output is also false"""
+
+
+val1 = True
+val2 = False
+print("OR Operator: ", val1 or val2) 
+
+""" a) if both values are true then the output is also true
+    b) if one of them is true then still output is true
+    c) if both of them are false then the output is false"""
