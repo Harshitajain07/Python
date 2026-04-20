8d0681070394308f2d0efec4180055822098343b
Harshitajain07 <harshitajain178@gmail.com>
2026-04-20 14:39:33 +0530

    Initial Commit

---
+++ Operators/ArithmeticOp.py
@@ -0,0 +1,35 @@
+#arithmetic operators (this is a single line comment)
+
+'''
+
+a = 1000
+b = 500
+sum = a+b
+print(sum)
+         (this is a multiline comment)   '''
+"""
+a = 1000
+b = 500
+diff = a-b
+print(diff) """
+
+
+"""
+a = 5
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b) """
+
+
+a = 4
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
---
+++ Operators/Assignmentop.py
@@ -0,0 +1,35 @@
+#Different assignment operators
+
+"""
+num = 10
+num+=10
+print("num: ",num)
+"""
+
+"""
+num = 10
+num-=10
+print("num: ",num)
+"""
+
+"""
+num = 10
+num*=5
+print("num: ",num)
+"""
+
+"""
+num = 10
+num/=5
+print("num: ",num)
+"""
+
+"""
+num = 10
+num%=5
+print("num: ",num)
+"""
+
+num = 10
+num**=5
+print("num: ",num)
