ea076f03a0339f6d5015162e22b7cb0f69dc04ba
Harshitajain07 <harshitajain178@gmail.com>
2026-04-20 18:01:21 +0530

    Initial Commit

--- Operators/LogicalOp.py
+++ Operators/LogicalOp.py
@@ -21,11 +21,19 @@ print("AND Operator: ", val1 and val2)
 """a) if both values are true then the output is also true
     b) if any one of them is false then output is also false"""
 
-
+"""
 val1 = True
 val2 = False
 print("OR Operator: ", val1 or val2) 
+"""
 
 """ a) if both values are true then the output is also true
     b) if one of them is true then still output is true
     c) if both of them are false then the output is false"""
+
+
+a = 50
+b = 30
+print("OR Operator: ", (a==b) or (a>b))
+print("OR Operator: ", (a>=b) or (a>b))
+print("OR Operator: ", (a==b) or (a<b))
