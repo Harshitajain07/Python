d9673fd8d41642ac7d163463985b09ae1175acb9
Harshitajain07 <harshitajain178@gmail.com>
2026-05-27 17:12:27 +0530

    Initial Commit

---
+++ InputsInPython.py
@@ -0,0 +1,24 @@
+#1. input() statement is used to accept values using keyword from users 
+
+""" input("Enter your name: ") """
+
+"""
+name = input("Enter your name: ")
+print("Welcome", name)  """
+
+"""
+val = input("Enter some values: ")
+print(type(val), val) """
+
+
+""" 2. int(input()) in this whatever is the exact type of value it only prints that . in this it 
+doesnt mean if the value is string it gives type  "int".... if the value is int it also give type as 
+int"""
+
+"""
+val = int(input("Enter some value: "))
+print(type(val), val)
+"""
+
+val = float(input("Enter some value: "))
+print(type(val), val)
