724fc72f35b3c7000a633119582dcc14fe0f56c7
Harshitajain07 <harshitajain178@gmail.com>
2026-05-27 18:07:02 +0530

    Initial Commit

---
+++ FloatNosAverage.py
@@ -0,0 +1,8 @@
+#WAP to print two floating point numbers and print their average 
+
+first = float(input("Enter First float number: "))
+second = float(input("Enter Second float number: "))
+
+average  = (first+second)/2
+
+print("Average float number is: ", average)
