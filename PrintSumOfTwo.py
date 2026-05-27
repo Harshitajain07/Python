34457986587ad94b60f6372501d6d3948a7378d0
Harshitajain07 <harshitajain178@gmail.com>
2026-05-27 17:28:37 +0530

    Initial Commit

---
+++ PrintSumOfTwo.py
@@ -0,0 +1,8 @@
+#WAP TO INPUT TWO NUMBERS AND PRINT THEIR SUM 
+
+
+first = int(input("Enter First Number: "))
+second = int(input("Enter Second Number: "))
+
+sum = first+second 
+print("Sum of 2 numbers: ", sum)
