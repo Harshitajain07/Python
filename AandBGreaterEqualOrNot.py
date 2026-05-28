b638fbb79bdc163ae6a4b38a71b61a12e96072be
Harshitajain07 <harshitajain178@gmail.com>
2026-05-28 16:26:17 +0530

    Initial Commit

---
+++ AandBGreaterEqualOrNot.py
@@ -0,0 +1,6 @@
+#WAP to inout 2 integer numbers A and B. Print true if A is greater thanor equal to B. If not, print false 
+
+a = int(input("Enter First number: "))
+b = int(input("Enter second number:  "))
+
+print(a>=b)
