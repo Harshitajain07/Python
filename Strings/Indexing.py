f4bd2b6db785dd2dfa813f704547799c6e232877
Harshitajain07 <harshitajain178@gmail.com>
2026-06-19 01:29:48 +0530

    Initial Commit

---
+++ Strings/Indexing.py
@@ -0,0 +1,12 @@
+#In indexing we access the characters with their position in strings 
+
+"""
+str = "Apna College"
+ch = str[3]
+print(ch) """
+
+str = "Apna College"
+print(str[4])
+
+
+#we can do it in any type. here in second one it prints empry space because in 4th position there is empty space.
