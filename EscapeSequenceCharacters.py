efac9b7e3835108d834eed584193d29f6a23734d
Harshitajain07 <harshitajain178@gmail.com>
2026-06-18 14:56:29 +0530

    Fix dashboard page

---
+++ EscapeSequenceCharacters.py
@@ -0,0 +1,9 @@
+#Escape sequence characters in which we use tab and new 
+
+str1 = "This is a string.\nWe are creating it in Python"
+print(str1)
+
+
+
+str2 = "This is a string.\tWe are creating it in Python"
+print(str2)
