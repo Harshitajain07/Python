a5bb8a0458a5a2e3268aabb1e3a51b126471da35
Harshitajain07 <harshitajain178@gmail.com>
2026-06-29 14:04:11 +0530

    Initial Commit

---
+++ Strings/Slicing.py
@@ -0,0 +1,17 @@
+#slicing it access parts of a string 
+
+"""
+str = "Apna College"
+print(str[1:4])
+"""
+"""
+str = "Apna College"
+print(str[0:7])
+"""
+"""
+str = "Apna College"
+print(str[5:12])
+"""
+
+str = "Apna College"
+print(str[:4])
